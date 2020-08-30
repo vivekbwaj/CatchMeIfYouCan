1e1a25e68657aa2aa7d3c629050db54d69c4ce89
vivek bhardwaj <vivek.bhardwaj@beteasy.com.au>
2020-08-30 23:43:50 +1000

    add response headers

--- mockBendigo.spec.js
+++ mockBendigo.spec.js
@@ -11,13 +11,15 @@ const playwright = require ('playwright');
 
     await page.route('**/api/notifications', route => {
         const request = route.request()
-        console.log(request.url(),JSON.stringify(request.headers()));
+        console.log(request.url());
         // return route.continue();
         return route.fulfill(
             {
                 status: 200,
-                headers:{"accept":"*/*","x-csrf-token":"7wwHcW7EmZRV92GOFgdea5CghZKihViQQpUjyp9SsAd/POKcFdIKL0ghZo0J6z1+MVszzDU35zLXzCjOlqdivQ==","user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4217.0 Safari/537.36","x-requested-with":"XMLHttpRequest","referer":"https://demo.bendigobank.com.au/banking/","cookie":"_device_id=a712080a-4ec8-4c68-a124-e517a7b99863; _session_id=8c14ea8051a9a4fdb2c33e5d4900c335"},
-                contentType: 'application/json',
+                headers:{'content-type': 'application/json','Referrer-Policy': 'origin-when-cross-origin, strict-origin-when-cross-origin','X-Permitted-Cross-Domain-Policies':'none',
+                'Strict-Transport-Security': 'max-age=31536000; includeSubdomains','Expires': 0
+            
+            },
                 body: {
                     "upcoming":0,
                     "upcomingForAnother":0,
@@ -35,6 +37,6 @@ const playwright = require ('playwright');
 
     await page.waitForLoadState("domcontentloaded"); // The promise resolves after 'load' event.
 
-    await page.waitForTimeout(30000);
+    await page.waitForTimeout(60000);
     await browser.close();
 })();
